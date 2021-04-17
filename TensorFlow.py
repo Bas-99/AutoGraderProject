0f26069610d4b12faf6efd35b9d4176964a8989e
Bas van de Rijt <b.v.d.rijt@student.tue.nl>
2021-04-17 22:28:28 +0200

    grading function fixed

--- TensorFlow.py
+++ TensorFlow.py
@@ -1,32 +1,51 @@
 # https://www.tensorflow.org/install/gpu
 from tensorflow.keras.models import load_model
-from OldFiles.Converting_Data_prod1 import X, classes
+from Video2Frames import create_data
 import numpy as np
+import os
 
-model_path = 'C:\\Users\\20182615\\Documents\\Jaar 3\\BEP\\saved_ML_models\\model_1_pickup'
+model_path = 'C:\\Users\\20182615\\Documents\\Jaar 3\\BEP\\EncodingDecoding\\grading\\models'
+simulation_path = "C:\\Users\\20182615\\Documents\\Jaar 3\\BEP\\EncodingDecoding\\grading\\simulations"
+test_names = ["test1a","test1b","test2","test3a","test3b"]
+seq_len = [250,350,350,350,350]
 
-new_model = load_model(model_path, compile=True)
+def last3(s):
+    return s[-7:-4]
 
-# new_model.summary()
-# loss, acc = new_model.evaluate(X, Y, verbose=2)
-# print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
-# print(new_model.predict(X).shape)
+def gradingData():
+    classes = ["correct","incorrect"]
+    for name in test_names:
+        test_path = os.path.join(simulation_path, name)
+        file_path = os.path.join(model_path, name)
+        new_model = load_model(file_path, compile=True)
 
-score = 0
-for i in range(len(X)):
-    samples_to_predict = []
-    samples_to_predict.append(X[i])
-    samples_to_predict = np.array(samples_to_predict)
+        X = create_data(test_path,seq_len[test_names.index(name)])
+        count = 0
+        for x in X:
+            score = 0
+            samples_to_predict = []
+            samples_to_predict.append(x)
 
-    predictions = new_model.predict(samples_to_predict)
-    classif = np.argmax(predictions, axis=1)
+            samples_to_predict = np.array(samples_to_predict)
+            # print(samples_to_predict.shape)
 
-    binary = classes[classif[0]]
-    print(predictions)
-    if binary == "correct":
-        score += 1
+            predictions = new_model.predict(samples_to_predict)
 
-finalScore = score/len(X)
-print("Average score for picking product 1: "+str(finalScore)+" (maximum of 1.0)")
+            classif = np.argmax(predictions, axis=1)
+
+            files = []
+            for file in os.listdir(test_path):
+                if file.endswith(".avi"):
+                    files.append(file)
+
+            if classes[classif[0]] == "correct":
+                score += 1
+            print("Group " + last3(files[count]) + " scored " + str(score) + " point for " + name)
+            count += 1
+
+
+gradingData()
+
+
 
 
